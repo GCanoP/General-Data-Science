4fbad49043875bcbab44a0d0de3062569ce170a2
Gerardo Cano <gerardocano18@gmail.com>
2023-01-27 02:50:55 -0600

    Machine_Learning_UDEMY_Course_Section_1_Updated_R_File_Updated

--- Machine Learning/UDEMY Course Machine Learning/Section 1 Data Preprocessing/GCP_Data_Preprocessing_Basic_Template.R
+++ Machine Learning/UDEMY Course Machine Learning/Section 1 Data Preprocessing/GCP_Data_Preprocessing_Basic_Template.R
@@ -2,7 +2,7 @@
 # Machine Learning Course UDEMY - Gerardo Cano Perea. 
 # Basic Template for Data Managment. 
 
-# Import Dataset. 
+# Import dataset. 
 dataset = read.csv('Data.csv')
 
 # Replace NA values with the mean value of the column. 
@@ -17,5 +17,28 @@ dataset$Salary = ifelse(is.na(dataset$Salary),
 dataset$Country = factor(dataset$Country, 
                          levels = c("France", "Spain", "Germany"),
                          labels = c(1, 2, 3))
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c(unique(dataset$Purchased)),
+                           labels = c(sequence(length(unique(dataset$Purchased)))))
+
+# Split training and test datasets. 
+# install.packages("caTools")
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+testing_set = subset(dataset, split == FALSE)
+
+# Scaling Values.
+training_set[,2:3] = scale(training_set[,2:3])
+testing_set[,2:3]= scale(testing_set[,2:3])
+
+
+
+
+
+
+
+
 
 
